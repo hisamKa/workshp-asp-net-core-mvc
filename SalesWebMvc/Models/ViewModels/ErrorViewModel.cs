90c0d9a57461dbb4691bfd26421846d0027e3669
Karen Santos <hisamitsu.moodle@gmail.com>
2019-12-19 17:50:16 -0300

    Returning custom error page

--- SalesWebMvc/Models/ViewModels/ErrorViewModel.cs
+++ SalesWebMvc/Models/ViewModels/ErrorViewModel.cs
@@ -5,6 +5,8 @@ namespace SalesWebMvc.Models.ViewModels
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
+        public string RequestID { get; internal set; }
+        public string Message { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
