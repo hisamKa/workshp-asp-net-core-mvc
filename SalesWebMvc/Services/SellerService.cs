fc165e4b4a2bf6a5c80d0fc6a9e9ba32ca03d245
Karen Santos <hisamitsu.moodle@gmail.com>
2019-12-19 21:50:38 -0300

    Exception handling for delete (referential integrity)

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -12,51 +12,57 @@ namespace SalesWebMvc.Services
     {
         private readonly SalesWebMvcContext _context;
 
-        //Construtor
         public SellerService(SalesWebMvcContext context)
         {
             _context = context;
         }
 
-        //Retornar uma lista com todos os vendedores do BD
         public async Task<List<Seller>> FindAllAsync()
         {
-            return await _context.Seller.ToListAsync();//Seller -> acessa a fonte de dados relacionada a tabela de vendedores e converte para lista
+            return await _context.Seller.ToListAsync();
         }
 
         public async Task InsertAsync(Seller obj)
         {
             _context.Add(obj);
-           await _context.SaveChangesAsync();
+            await _context.SaveChangesAsync();
         }
-        public async Task <Seller> FindByIdAsync(int id)
+
+        public async Task<Seller> FindByIdAsync(int id)
         {
             return await _context.Seller.Include(obj => obj.Department).FirstOrDefaultAsync(obj => obj.Id == id);
         }
+
         public async Task RemoveAsync(int id)
         {
-            var obj = await _context.Seller.FindAsync(id);
-            _context.Seller.Remove(obj);//removeu do dbset 
-            await _context.SaveChangesAsync();
+            try
+            {
+                var obj = await _context.Seller.FindAsync(id);
+                _context.Seller.Remove(obj);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException e)
+            {
+                throw new IntegrityException("Can't delete seller because he/she has sales");
+            }
         }
 
         public async Task UpdateAsync(Seller obj)
         {
             bool hasAny = await _context.Seller.AnyAsync(x => x.Id == obj.Id);
-            if (!hasAny)//Any - > serve para ver se existe algum registro no BD
+            if (!hasAny)
             {
                 throw new NotFoundException("Id not found");
             }
             try
             {
                 _context.Update(obj);
-               await _context.SaveChangesAsync();
+                await _context.SaveChangesAsync();
             }
-            catch(DbConcurrencyException e)
+            catch (DbUpdateConcurrencyException e)
             {
                 throw new DbConcurrencyException(e.Message);
             }
-                    
         }
     }
 }
